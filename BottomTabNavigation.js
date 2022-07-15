730869fda5f616823bce91647f0efabf4ca0861e
Apple2019md <65364711+Apple2019md@users.noreply.github.com>
2022-07-15 13:52:52 -0500

    Add files via upload

---
+++ BottomTabNavigation.js
@@ -0,0 +1,41 @@
+import React from "react"; 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottom, TabNavigator } from '@react-navigation/bottom-tabs'; 
+import Ionicons from 'react-native-vector-icons/Ionicons'; 
+import Feed from '../screens/Feed';
+import CreatPost from '../screens/CreatPost';
+
+
+
+
+const Tab = createBottomTabNavigator();
+const BottomTabNavigator = () => {
+ 
+return (
+  <Tab.Navigator
+  screenOptions={({ route })} => ({
+    tabBarIcon: ({ focused, color, size}) => {
+      let iconName;
+      if(route.name === 'Feed') {
+        iconName = focused
+                ? 'book'
+                :'book-outline';
+      } else if (route.name === 'CreatPost') {
+        iconName = focused ? 'create' : 'create-outline';
+            }
+            return <Ionicons name={iconName} size={size} color={color}/>; 
+      },
+    })}
+    tabBarOptions={{ 
+      activeTintColor : 'tomato',
+      inactiveTintColor: 'gray',
+    }}
+    >
+      <Tab.Screen name="Feed" component={Feed}/>
+      <Tab.Screen name="CreatPost" component={CreatPost}/>
+    </Tab.Navigator>
+  );
+}
+ 
+export default BottomTabNavigator
+
